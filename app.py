ab3b1c2da04cb9e46cd61a57637e1d290c00cd03
marco.veronesi <marco.veronesi@keyos.it>
2023-03-25 17:09:55 +0100

    first demo

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request, send_file
+import yt_dlp
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/api/download')
+def download():
+    url = request.args.get('q')
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'outtmpl': 'audio.mp3',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a'
+        }]
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return send_file('audio.mp3', as_attachment=True)
